317824861c9f108e7b5f3ad4321d0f20dd6cc358
luka-kirvalidze1 <pubgapubga90@gmail.com>
2026-08-03 00:40:17 +0400

    added swaylock config

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-option=$(printf "Shutdown\nReboot\nLogout" | rofi -dmenu -p "Powermenu" -theme "~/dotfiles/rofi/.config/rofi/powermenu.rasi")
+option=$(printf "Shutdown\nReboot\nLogout\nLock" | rofi -dmenu -p "Powermenu" -theme "~/dotfiles/rofi/.config/rofi/powermenu.rasi")
 
 case "$option" in
     "Shutdown")poweroff;;
     "Reboot")reboot;;
     "Logout")loginctl terminate-user $USER;;
+    "Lock")swaylock;;
 esac
