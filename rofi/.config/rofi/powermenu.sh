e02ab4c6b78790332aadb9bb840a0fc7ec70b2a6
luka-kirvalidze1 <pubgapubga90@gmail.com>
2026-08-03 00:27:51 +0400

    added simple rofi powermenu script

---
+++ rofi/.config/rofi/powermenu.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+option=$(printf "Shutdown\nReboot\nLogout" | rofi -dmenu -p "Powermenu" -theme "~/dotfiles/rofi/.config/rofi/powermenu.rasi")
+
+case "$option" in
+    "Shutdown")poweroff;;
+    "Reboot")reboot;;
+    "Logout")loginctl terminate-user $USER;;
+esac
